c04595c033f8966d85f68576c7aa34f77ee5b2ad
Dawei Dai <shachopin@gmail.com>
2016-11-17 14:41:22 -0800

    11/16/2016 2:41PM

--- server.js
+++ server.js
@@ -17,11 +17,14 @@ app.use(bodyParser.urlencoded({extended: true})); //to allow image or video, fal
 app.use(bodyParser.json());
 app.use(morgan('dev')); //log request info in the console
 
+app.use(express.static(__dirname + '/public'));  
+//has to before the line rendering index.html, otherwise the relative links reference (css, js) on index.html would fail
+//meaning start from current folder and /public, anything inside will not go through node.js route, but static asset
 var api = require('./app/routes/api')(app, express);
 app.use('/api', api); //this is for the API have this prefix: /api - for example, localhost:3000/api/signup
 //type anything else will go to the next route, to show the below html page: for example, if I tpe localhost:3000/api/userss
 app.get('*', function(req,res){
-  res.sendFile(__dirname + '/public/views/index.html');
+  res.sendFile(__dirname + '/public/app/views/index.html');
 });
 
 app.listen(config.port, function(err){
