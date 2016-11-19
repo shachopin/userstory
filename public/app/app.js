7fab92d0db4f25bf2bcc56c66f09d74862baa8a6
Dawei Dai <shachopin@gmail.com>
2016-11-18 17:25:31 -0800

    11/18/2016 final

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,6 @@
-angular.module('MyApp', ['appRoutes', 'authService', 'mainCtrl', 'userCtrl', 'userService']);
+angular.module('MyApp', ['appRoutes', 'authService', 'mainCtrl', 'userCtrl', 'userService'])
+.config(function($httpProvider) {
+
+  $httpProvider.interceptors.push('AuthInterceptor');
+
+});
