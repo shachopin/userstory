5338cf233f91d2c779da9bfc9c7eb57a187326c9
Dawei Dai <shachopin@gmail.com>
2016-11-18 17:27:36 -0800

    11/18/2016 final

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,7 @@
 angular.module('MyApp', ['appRoutes', 'authService', 'mainCtrl', 'userCtrl', 'userService'])
 .config(function($httpProvider) {
 
-  $httpProvider.interceptors.push('AuthInterceptor');
+  $httpProvider.interceptors.push('AuthInterceptor'); 
+  //without this, all the /me api would fail due to no token is sent in the header
 
 });
